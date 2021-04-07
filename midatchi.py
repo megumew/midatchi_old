68d6d97cc812ebf16f31427404d4c62725e66415
megumew <megumeww@gmail.com>
2021-04-07 05:01:31 -0400

    Update midatchi.py
    Added a pet naming feature. Pets name can be displayed with shift key.

--- midatchi.py
+++ midatchi.py
@@ -13,35 +13,56 @@ running = True
 blink = False
 updateScreen = False
 titleScreen = True
-
+petNameScreen = True
+nameNotConfirm = True
+petName = ""
 pet = "(o w o)"
 petBlink = "(- w -)"
 
 print("WELCOME TO MIDATCHI!\n~~~~~~~~~~~~~~~~~~~\npress ENTER to play")
 while titleScreen:
     if keyboard.is_pressed('enter'):
+        input.__format__
         titleScreen = False
+
+print("Please enter your pets name:")
+while petNameScreen:
+    nameNotConfirm = True
+    petName = input()
+    if petName != "":
+        while nameNotConfirm:
+            print("Are you sure you want your pet to be named " + petName + "? ('yes'/'no')")
+            yesNo = str.lower(input())
+            if yesNo == "yes" :
+                petNameScreen = False
+                nameNotConfirm = False
+                break
+            elif yesNo == "no":
+                nameNotConfirm = False
+                
 
 
 while running:
+
     now = datetime.now()
     timeLastTick = current_time
     current_time = now.strftime("%H:%M:%S")
+
     if current_time != timeLastTick:
         blink = not blink
         updateScreen = True
         cls()
-    if keyboard.is_pressed('shift'):
-        print("Current Time =", current_time)
-        userInput = input()
-    
 
-    if blink:
-        if updateScreen:
+    if keyboard.is_pressed('shift'):
+        cls()
+        print(petName)
+
+
+    if updateScreen:
+        if blink:
             updateScreen = False
             print(petBlink + "\n" + current_time)
-    else:
-        if updateScreen:
+        else:
             updateScreen = False
             print(pet + "\n" + current_time)
 
