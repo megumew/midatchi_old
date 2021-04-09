e7d284d0dd718e65ca3962222fcfbcfe7db8e3ef
megumew <megumeww@gmail.com>
2021-04-09 01:26:04 -0400

    Update midatchi.py
    Displays user input over pet's head.

--- midatchi.py
+++ midatchi.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import sys
 import keyboard
 import os
 
@@ -26,12 +27,13 @@ petName = ""
 petEyes = "(o w o)"
 petBlink = "(- w -)"
 pet = petEyes
+userInput = ""
 
 
 def render():
-    print(petName + ":\t" + statusHunger + " " + statusFun + " " + status3 + "\n\n\t" + pet + "\n\t" + current_time)
+    print(petName + ":\t" + statusHunger + " " + statusFun + " " + status3 + "\n\t"+ userInput +"\n\t" + pet + "\n\t" + current_time)
 
-def petState():
+def petAnimate():
     global pet
     global blink
     if(blink):
@@ -42,7 +44,8 @@ def petState():
 def tick():
     global blink
     blink = not blink
-    petState()
+    petAnimate()
+
 
 titleScreen = True
 print("WELCOME TO MIDATCHI!\n~~~~~~~~~~~~~~~~~~~\npress ENTER to play")
@@ -68,20 +71,22 @@ while petNameScreen:
             elif yesNo == "no":
                 nameNotConfirm = False
 
-
 while running:
     now = datetime.now()
     timeLastTick = current_time
     current_time = now.strftime("%H:%M:%S")
 
     if current_time != timeLastTick:
+        
         tick()
         cls()
         render()
 
-    if keyboard.is_pressed('shift'):
+    if keyboard.is_pressed('/'):
         cls()
-        print(petName)
+        input.__format__
+        print("Input: ")
+        userInput = input()
 
     if keyboard.is_pressed('escape'):
         exit()
